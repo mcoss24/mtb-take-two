9f00ba0d8f692784530f57b0e3d645d3082a9d9e
mcoss24 <mcoss24@yahoo.com>
2021-05-25 22:28:44 +0000

    made some updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,14 +15,14 @@ class PagesController < ApplicationController
     
         
     @season = @list.at(0) 
-    @geography = params.fetch("geography").capitalize
-    @climate = params.fetch("climate").capitalize
+    @geography = params.fetch("geography", false)
+    @climate = params.fetch("climate", false)
     @type = @list.at(-3)
+
 
     @query1 = Spot.where({@season => true}).where({:geography => @geography}).where({ :climate => @climate}).where({@type => true}) # get the record the based on the 
                                                                                                                                     # based on the user's prefs
-    #@photo = Photo.where({:spot_id => })
-
+    
     if @query1.first == nil 
       
       redirect_to("/plan", { :alert => "The criteria selected do not match a destination. Please try again!"})
